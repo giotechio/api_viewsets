76475e6dab39b8d9abedfbae128f21c13bfa8f08
giotechio <g.abdusheli@outlook.com>
2023-05-01 21:04:25 +0400

    project implemented with ViewSets

--- postings/urls.py
+++ postings/urls.py
@@ -1,14 +1,55 @@
 from django.urls import path
 from postings import views
 
+from postings.views import PostingViewSet, UserViewSet, api_root
+
+
+
+# urlpatterns = [
+#     path("postings/", views.PostingList.as_view(), name='posting-list'),
+#     path("postings/<int:pk>/", views.PostingDetail.as_view(), name='posting-detail'),
+
+#     path("users/", views.UserList.as_view(), name='user-list'),
+#     path("users/<int:pk>/", views.UserDetail.as_view(), name='user-detail'),
+#     path("", views.api_root),
+# ]
+
+
+
+"""Using ViewSets"""
+
+posting_list = PostingViewSet.as_view(
+    {
+    'get': 'list',
+    'post': 'create'
+}
+)
+
+posting_detail = PostingViewSet.as_view(
+    {
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+}
+)
+user_list = UserViewSet.as_view(
+    {
+    'get': 'list'
+}
+)
+
+user_detail = UserViewSet.as_view(
+    {
+    'get': 'retrieve'
+}
+)
 
 urlpatterns = [
-    path("postings/", views.PostingList.as_view(), name='posting-list'),
-    path("postings/<int:pk>/", views.PostingDetail.as_view(), name='posting-detail'),
+    path("postings/", posting_list, name='posting-list'),
+    path("postings/<int:pk>/", posting_detail, name='posting-detail'),
 
-    path("users/", views.UserList.as_view(), name='user-list'),
-    path("users/<int:pk>/", views.UserDetail.as_view(), name='user-detail'),
-    path("", views.api_root),
+    path("users/", user_list, name='user-list'),
+    path("users/<int:pk>/", user_detail, name='user-detail'),
+    path("", api_root),
 ]
-
-
--- postings/views.py
+++ postings/views.py
@@ -8,6 +8,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response  
 from rest_framework.reverse import reverse  
 
+from rest_framework import viewsets     # needed to refactor using viewsets
+
 
 # root endpoint
 
@@ -20,32 +22,52 @@ def api_root(request, format=None):
         }
     )
 
+"""POSTINGS"""
 
-# ListCreateAPIView to create a read-write endpoint that lists all available 'Posting' instances
+# """ ListCreateAPIView to create a read-write endpoint that lists all available 'Posting' instances"""
 
-class PostingList(generics.ListCreateAPIView):
+# class PostingList(generics.ListCreateAPIView):
+#     queryset = Posting.objects.all()
+#     serializer_class = PostingSerializer
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+#     def perform_create(self, serializer):
+#         serializer.save(creator=self.request.user)
+
+
+# """ RetrieveUpdateDestroyAPIView for a read-write-delete endpoint for each individual Posting """
+# class PostingDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Posting.objects.all()
+#     serializer_class = PostingSerializer
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsCreatorOrReadOnly,)
+
+"""viewset for `list`, `create`, `retrieve`,`update` and `destroy` actions"""
+
+class PostingViewSet(viewsets.ModelViewSet):
     queryset = Posting.objects.all()
     serializer_class = PostingSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsCreatorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
 
 
-# RetrieveUpdateDestroyAPIView for a read-write-delete endpoint for each individual Posting
-class PostingDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Posting.objects.all()
-    serializer_class = PostingSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsCreatorOrReadOnly,)
+
+"""USERS""" 
+
+# class UserList(generics.ListAPIView):  
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
 
-# Users 
-
-class UserList(generics.ListAPIView):  
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserDetail(generics.RetrieveAPIView):  
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):  
+"""viewset fo list and retrive operations"""
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    
     queryset = User.objects.all()
     serializer_class = UserSerializer
